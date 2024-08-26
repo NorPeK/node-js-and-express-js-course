3bd6aac38b824ca406bcccac761c3f3366ebd41e
NorPeK <norpekbusiness@hotmail.com>
2024-08-26 19:33:39 +0300

    alternative syntax for modules

--- modules.js
+++ modules.js
@@ -2,8 +2,10 @@
 //Modules are encapsulated code (only shar minimum)
 const names = require("./names");
 const sayHi = require("./utils");
+const data = require("./alternative-flavor");
 console.log(names);
 console.log(sayHi);
+console.log(data);
 sayHi("NorPeK");
 sayHi(names.john);
 sayHi(names.peter);
