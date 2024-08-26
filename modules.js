03a87b28deed800c90ee77941680123b08767944
NorPeK <norpekbusiness@hotmail.com>
2024-08-26 19:39:42 +0300

    mind grenades

--- modules.js
+++ modules.js
@@ -3,9 +3,10 @@
 const names = require("./names");
 const sayHi = require("./utils");
 const data = require("./alternative-flavor");
-console.log(names);
-console.log(sayHi);
-console.log(data);
-sayHi("NorPeK");
-sayHi(names.john);
-sayHi(names.peter);
+require("./mind-grenades");
+// console.log(names);
+// console.log(sayHi);
+// console.log(data);
+// sayHi("NorPeK");
+// sayHi(names.john);
+// sayHi(names.peter);
